7431378056cb9f47a4e8478f71d7dd87eda9ecd0
Sakamoto0525 <525masaya@gmail.com>
2020-06-17 22:03:04 +0900

    videosテーブル作成

---
+++ src/database/factories/VideosFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\videos;
+use Faker\Generator as Faker;
+
+$factory->define(videos::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
