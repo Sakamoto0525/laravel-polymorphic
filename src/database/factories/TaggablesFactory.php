4fd9c15335143c42a30472150ae409414571a3cf
Sakamoto0525 <525masaya@gmail.com>
2020-06-17 22:09:57 +0900

    taggablesテーブル作成

---
+++ src/database/factories/TaggablesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\taggables;
+use Faker\Generator as Faker;
+
+$factory->define(taggables::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
